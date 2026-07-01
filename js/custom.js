51d425c2235c5253c0bed6fa7266667ed6bda248
Roman <uixroman@gmail.com>
2026-07-01 20:12:27 +0300

    Исправил разметку1

--- js/custom.js
+++ js/custom.js
@@ -1,54 +1,49 @@
+ 
+
+window.scrollTo(0, 0);
+
 let lenis;
 let shouldStop = document.body.classList.contains("no-scroll");
 
 document.addEventListener("DOMContentLoaded", () => {
-  lenis = new Lenis({
-    smooth: true,
-    lerp: 0.05,
-  });
+ lenis = new Lenis({
+  smooth: true,
+  lerp: 0.05,
+ });
+ window.lenis = lenis;
 
-  window.lenis = lenis;
 
-  if (shouldStop) {
-    lenis.stop();
-  } else {
-    lenis.start();
 
-    // 👉 скролл наверх только если скролл активен
-    lenis.scrollTo(0, { immediate: true });
-  }
+ if (shouldStop) {
+  lenis.stop();
+ } else {
+  lenis.start();
+ }
 
-  lenis.on("scroll", ScrollTrigger.update);
+ lenis.on("scroll", ScrollTrigger.update);
+ gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+ });
+ gsap.ticker.lagSmoothing(0);
+ ScrollTrigger.refresh();
 
-  gsap.ticker.add((time) => {
-    lenis.raf(time * 1000);
-  });
 
-  gsap.ticker.lagSmoothing(0);
-
-  ScrollTrigger.refresh();
 });
+
 
 
 const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.attributeName === "class") {
-      shouldStop = document.body.classList.contains("no-scroll");
-
-      if (lenis) {
-        shouldStop ? lenis.stop() : lenis.start();
-
-
-        if (!shouldStop) {
-
-          ScrollTrigger.refresh();
-        }
-      }
-    }
-  });
+ mutations.forEach((mutation) => {
+  if (mutation.attributeName === "class") {
+shouldStop = document.body.classList.contains("no-scroll");
+if (lenis) {
+ shouldStop ? lenis.stop() : lenis.start();
+}
+  }
+ });
 });
 
 observer.observe(document.body, {
-  attributes: true,
-  attributeFilter: ["class"],
+ attributes: true,
+ attributeFilter: ["class"],
 });
